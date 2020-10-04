be5afb4689a00254aef12142ddd60dd4f99fd4ac
eddex <eddex@outlook.com>
2020-10-04 15:35:15 +0200

    refactor: use # for docstring instaed of """

--- addons/easy_charts/LineChart2D/LineChart2D.gd
+++ addons/easy_charts/LineChart2D/LineChart2D.gd
@@ -1,19 +1,17 @@
 tool
 extends Chart2D
 
-"""
-[Linechart2D] - General purpose node for Line Charts
-A line chart or line plot or line graph or curve chart is a type of chart which
-displays information as a series of data points called 'markers'
-connected by straight line segments.
-It is a basic type of chart common in many fields. It is similar to a scatter plot
-except that the measurement points are ordered (typically by their x-axis value)
-and joined with straight line segments.
-A line chart is often used to visualize a trend in data over intervals of time –
-a time series – thus the line is often drawn chronologically.
-In these cases they are known as run charts.
-/ source: Wikipedia /
-"""
+# [Linechart2D] - General purpose node for Line Charts
+# A line chart or line plot or line graph or curve chart is a type of chart which
+# displays information as a series of data points called 'markers'
+# connected by straight line segments.
+# It is a basic type of chart common in many fields. It is similar to a scatter plot
+# except that the measurement points are ordered (typically by their x-axis value)
+# and joined with straight line segments.
+# A line chart is often used to visualize a trend in data over intervals of time –
+# a time series – thus the line is often drawn chronologically.
+# In these cases they are known as run charts.
+# Source: Wikipedia
 
 
 var OutlinesTween: Tween
