d2b04f0953fb25da834d89bb5780ca34a231c716
Nicolò Santilio <n.santilio97@gmail.com>
2020-12-31 12:29:31 +0100

    Update plugin.gd

--- addons/easy_charts/plugin.gd
+++ addons/easy_charts/plugin.gd
@@ -4,7 +4,7 @@ extends EditorPlugin
 func _enter_tree():
 	add_autoload_singleton("Utilities","res://addons/easy_charts/Utilities/Scripts/utilities.gd")
 	add_custom_type("Chart","Control", load("res://addons/easy_charts/Utilities/Scripts/chart.gd"), preload("Utilities/icons/linechart.svg"))
-	add_custom_type("Chart2D","Node2D", preload("Utilities/Scripts/chart2d.gd"), preload("Utilities/icons/linechart2d.svg"))
+	add_custom_type("Chart2D","Node2D", load("res://addons/easy_charts/Utilities/Scripts/chart2d.gd"), preload("Utilities/icons/linechart2d.svg"))
 
 func _exit_tree():
 	remove_custom_type("Chart")
