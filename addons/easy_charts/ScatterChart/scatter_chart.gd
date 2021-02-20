cf2ef1cabd155fc31de7f5c5952a3aebb80b3c05
Jorge <63685920+JFerrerBeired@users.noreply.github.com>
2021-02-20 18:38:24 +0100

    Add missing features to ScatterChart
    I added the missing features from LineChart to ScatterChart (like treshold and some properties) in preparation to creation of a Parent Class common to both. This way the changes can be traced easily.

--- addons/easy_charts/ScatterChart/scatter_chart.gd
+++ addons/easy_charts/ScatterChart/scatter_chart.gd
@@ -21,6 +21,12 @@ func _get_property_list():
 		# Chart Properties
 		{
 			"hint": PROPERTY_HINT_NONE,
+			"usage": PROPERTY_USAGE_CATEGORY,
+			"name": "ScatterChart",
+			"type": TYPE_STRING
+		},
+		{
+			"hint": PROPERTY_HINT_NONE,
 			"usage": PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
 			"name": "Chart_Properties/are_values_columns",
 			"type": TYPE_BOOL
@@ -42,14 +48,14 @@ func _get_property_list():
 		# Chart Display
 		{
 			"hint": PROPERTY_HINT_RANGE,
-			"hint_string": "0.1,10",
+			"hint_string": "0.001, 10",
 			"usage": PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
 			"name": "Chart_Display/x_decim",
 			"type": TYPE_REAL
 		},
 		{
 			"hint": PROPERTY_HINT_RANGE,
-			"hint_string": "0.1,10",
+			"hint_string": "0.001, 10",
 			"usage": PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
 			"name": "Chart_Display/y_decim",
 			"type": TYPE_REAL
@@ -58,8 +64,9 @@ func _get_property_list():
 		# Chart Style
 		{ 
 			"hint": 24, 
-			"hint_string": "%d/%d:%s"%[TYPE_INT, PROPERTY_HINT_ENUM,
-			PoolStringArray(PointShapes.keys()).join(",")],
+			"hint_string": ("%d/%d:%s"
+			%[TYPE_INT, PROPERTY_HINT_ENUM,
+			PoolStringArray(PointShapes.keys()).join(",")]),
 			"name": "Chart_Style/points_shape", 
 			"type": TYPE_ARRAY, 
 			"usage": PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE
@@ -111,6 +118,12 @@ func _get_property_list():
 			"type": TYPE_COLOR
 		},
 		{
+			"hint": PROPERTY_HINT_NONE,
+			"usage": PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+			"name": "Chart_Style/use_template",
+			"type": TYPE_BOOL
+		},
+		{
 			"hint": PROPERTY_HINT_ENUM,
 			"hint_string": PoolStringArray(Utilities.templates.keys()).join(","),
 			"usage": PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
@@ -122,10 +135,23 @@ func _get_property_list():
 		{
 			"hint": PROPERTY_HINT_NONE,
 			"usage": PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+			"name": "Chart_Modifiers/treshold",
+			"type": TYPE_VECTOR2
+		},
+		{
+			"hint": PROPERTY_HINT_NONE,
+			"usage": PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+			"name": "Chart_Modifiers/only_disp_values",
+			"type": TYPE_VECTOR2
+		},
+		{
+			"hint": PROPERTY_HINT_NONE,
+			"usage": PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
 			"name": "Chart_Modifiers/invert_chart",
 			"type": TYPE_BOOL
 		},
 	]
+
 
 func structure_datas(database : Array):
 	# @labels_index can be either a column or a row relative to x values
@@ -205,19 +231,22 @@ func structure_datas(database : Array):
 	OFFSET.x = str(y_margin_max).length() * font_size
 	OFFSET.y = font_size * 2
 
+
 func build_chart():
-	SIZE = get_size()
+	SIZE = get_size() - Vector2(OFFSET.x, 0)
 	origin = Vector2(OFFSET.x, SIZE.y - OFFSET.y)
+
 
 func calculate_pass():
 	if show_x_values_as_labels:
-		x_chors = x_datas as PoolStringArray
+		x_chors = x_datas.duplicate(true) as PoolStringArray
 	else:
 		x_chors = x_labels
 	
 	# calculate distance in pixel between 2 consecutive values/datas
 	x_pass = (SIZE.x - OFFSET.x) / (x_chors.size() - 1 if x_chors.size() > 1 else x_chors.size())
-	y_pass = origin.y / (y_chors.size() - 1)
+	y_pass = (origin.y - ChartName.get_rect().size.y * 2) / (y_chors.size() - 1)
+
 
 func calculate_coordinates():
 	x_coordinates.clear()
@@ -233,7 +262,7 @@ func calculate_coordinates():
 	
 	if show_x_values_as_labels:
 		for x in x_datas.size():
-			x_coordinates.append(x_pass*x)
+			x_coordinates.append(x_pass * x)
 	else:
 		for x in x_datas.size():
 			x_coordinates.append((x_datas[x] - x_margin_min) * x_pass / h_dist)
@@ -246,16 +275,50 @@ func calculate_coordinates():
 		for y in y_coordinates[cluster].size():
 			if are_values_columns:
 				point_values[y].append([x_datas[cluster], y_datas[cluster][y]])
-				point_positions[y].append(Vector2(x_coordinates[cluster] + origin.x, origin.y - y_coordinates[cluster][y]))
+				point_positions[y].append(Vector2(x_coordinates[cluster] + origin.x,
+												  origin.y - y_coordinates[cluster][y]))
 			else:
 				point_values[cluster].append([x_datas[y], y_datas[cluster][y]])
-				point_positions[cluster].append(Vector2(x_coordinates[y] + origin.x, origin.y - y_coordinates[cluster][y]))
+				point_positions[cluster].append(Vector2(x_coordinates[y] + origin.x,
+														origin.y - y_coordinates[cluster][y]))
+
 
 func _draw():
 	clear_points()
 	draw_grid()
 	draw_chart_outlines()
+	draw_points()
+	draw_treshold()
+
+
+func draw_grid():
+	# ascisse
+	for p in x_chors.size():
+		var point : Vector2 = origin+Vector2((p)*x_pass,0)
+		# v grid
+		draw_line(point,point-Vector2(0,SIZE.y-OFFSET.y),v_lines_color,0.2,true)
+		# ascisse
+		draw_line(point-Vector2(0,5),point,v_lines_color,1,true)
+		draw_string(font,point+Vector2(-const_width/2*x_chors[p].length(),font_size+const_height),x_chors[p],font_color)
 	
+	# ordinate
+	for p in y_chors.size():
+		var point : Vector2 = origin-Vector2(0,(p)*y_pass)
+		# h grid
+		draw_line(point,point+Vector2(SIZE.x-OFFSET.x,0),h_lines_color,0.2,true)
+		# ordinate
+		draw_line(point,point+Vector2(5,0),h_lines_color,1,true)
+		draw_string(font,point-Vector2(y_chors[p].length()*const_width+font_size,-const_height),y_chors[p],font_color)
+
+
+func draw_chart_outlines():
+	draw_line(origin,SIZE-Vector2(0,OFFSET.y),box_color,1,true)
+	draw_line(origin,Vector2(OFFSET.x,0),box_color,1,true)
+	draw_line(Vector2(OFFSET.x,0),Vector2(SIZE.x,0),box_color,1,true)
+	draw_line(Vector2(SIZE.x,0),SIZE-Vector2(0,OFFSET.y),box_color,1,true)
+
+
+func draw_points():
 	var defined_colors : bool = false
 	if function_colors.size():
 		defined_colors = true
@@ -277,27 +340,11 @@ func _draw():
 			
 			PointContainer.add_child(point)
 
-func draw_grid():
-	# ascisse
-	for p in x_chors.size():
-		var point : Vector2 = origin+Vector2((p)*x_pass,0)
-		# v grid
-		draw_line(point,point-Vector2(0,SIZE.y-OFFSET.y),v_lines_color,0.2,true)
-		# ascisse
-		draw_line(point-Vector2(0,5),point,v_lines_color,1,true)
-		draw_string(font,point+Vector2(-const_width/2*x_chors[p].length(),font_size+const_height),x_chors[p],font_color)
-	
-	# ordinate
-	for p in y_chors.size():
-		var point : Vector2 = origin-Vector2(0,(p)*y_pass)
-		# h grid
-		draw_line(point,point+Vector2(SIZE.x-OFFSET.x,0),h_lines_color,0.2,true)
-		# ordinate
-		draw_line(point,point+Vector2(5,0),h_lines_color,1,true)
-		draw_string(font,point-Vector2(y_chors[p].length()*const_width+font_size,-const_height),y_chors[p],font_color)
 
-func draw_chart_outlines():
-	draw_line(origin,SIZE-Vector2(0,OFFSET.y),box_color,1,true)
-	draw_line(origin,Vector2(OFFSET.x,0),box_color,1,true)
-	draw_line(Vector2(OFFSET.x,0),Vector2(SIZE.x,0),box_color,1,true)
-	draw_line(Vector2(SIZE.x,0),SIZE-Vector2(0,OFFSET.y),box_color,1,true)
+func draw_treshold():
+	if v_dist != 0:
+		treshold_draw = Vector2((treshold.x * x_pass) + origin.x ,origin.y - ((treshold.y * y_pass)/v_dist))
+		if treshold.y != 0:
+			draw_line(Vector2(origin.x, treshold_draw.y), Vector2(SIZE.x, treshold_draw.y), Color.red, 0.4, true)
+		if treshold.x != 0:
+			draw_line(Vector2(treshold_draw.x, 0), Vector2(treshold_draw.x, SIZE.y - OFFSET.y), Color.red, 0.4, true)
