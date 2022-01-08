e601e6313922be6764784330a29d4a64d715a4ee
fenix-hub <n.santilio97@gmail.com>
2022-01-08 19:08:45 +0100

    fix min errors

--- addons/easy_charts/3d_charts/ScatterChart3D/scatter_chart3D.gd
+++ addons/easy_charts/3d_charts/ScatterChart3D/scatter_chart3D.gd
@@ -18,8 +18,6 @@ onready var PlaceholderPoint = $Chart/Point
 onready var Space = $ImmediateGeometry
 onready var DataTooltip = $DataTooltip/DataTooltip
 
-var point_node : PackedScene = preload("res://addons/easy_charts/utilities/components/point/point.gd")
-var FunctionLegend : PackedScene = preload("res://addons/easy_charts/utilities/containers/legend/function_legend.gd")
 
 var font_size : float = 16
 var const_height : float = font_size/2*font_size/20
@@ -113,42 +111,42 @@ signal chart_plotted(chart)
 signal point_pressed(point)
 
 func _point_plotted():
-    pass
+	pass
 
 func _ready():
-    pass
+	pass
 
 func _set_size(size : Vector2):
-    SIZE = size
+	SIZE = size
 #	build_chart()
 
 func clear():
-    pass
+	pass
 
 func load_font():
-    if font != null:
-        font_size = font.get_height()
-        var theme : Theme = Theme.new()
-        theme.set_default_font(font)
-        DataTooltip.set_theme(theme)
-    else:
-        var lbl = Label.new()
-        font = lbl.get_font("")
-        lbl.free()
-    if bold_font != null:
-        DataTooltip.Data.set("custom_fonts/font",bold_font)
+	if font != null:
+		font_size = font.get_height()
+		var theme : Theme = Theme.new()
+		theme.set_default_font(font)
+		DataTooltip.set_theme(theme)
+	else:
+		var lbl = Label.new()
+		font = lbl.get_font("")
+		lbl.free()
+	if bold_font != null:
+		DataTooltip.Data.set("custom_fonts/font",bold_font)
 
 func _plot(source_ : String, delimiter_ : String, are_values_columns_ : bool, x_values_index_ : int):
-    randomize()
-    
-    clear()
-    
-    load_font()
-    DataTooltip.hide()
-    
-    datas = read_data(source_,delimiter_)
+	randomize()
+	
+	clear()
+	
+	load_font()
+	DataTooltip.hide()
+	
+	datas = read_data(source_,delimiter_)
 #	count_functions()
-    structure_data(datas,are_values_columns_,x_values_index_)
+	structure_data(datas,are_values_columns_,x_values_index_)
 #	build_chart()
 #	calculate_pass()
 #	calculate_coordinates()
@@ -156,154 +154,154 @@ func _plot(source_ : String, delimiter_ : String, are_values_columns_ : bool, x_
 #	draw_chart()
 #
 #	create_legend()
-    emit_signal("chart_plotted", self)
+	emit_signal("chart_plotted", self)
 
 func plot():
-    randomize()
-    
-    clear()
-    
-    load_font()
-    DataTooltip.hide()
-    
-    if source == "" or source == null:
-        ECUtilities._print_message("Can't plot a chart without a Source file. Please, choose it in editor, or use the custom function _plot().",1)
-        return
-    datas = read_data(source,delimiter)
+	randomize()
+	
+	clear()
+	
+	load_font()
+	DataTooltip.hide()
+	
+	if source == "" or source == null:
+		ECUtilities._print_message("Can't plot a chart without a Source file. Please, choose it in editor, or use the custom function _plot().",1)
+		return
+	datas = read_data(source,delimiter)
 #	count_functions()
-    structure_data(datas,are_values_columns,x_values_index)
+	structure_data(datas,are_values_columns,x_values_index)
 #	build_chart()
 #	calculate_pass()
 #	calculate_coordinates()
 #	calculate_colors()
 #	draw_chart()
-    
+	
 #	create_legend()
-    emit_signal("chart_plotted", self)
+	emit_signal("chart_plotted", self)
 
 
 func read_data(source : String, delimiter : String):
-    var file : File = File.new()
-    file.open(source,File.READ)
-    var content : Array
-    while not file.eof_reached():
-        var line : PoolStringArray = file.get_csv_line(delimiter)
-        content.append(line)
-    file.close()
-    for data in content:
-        if data.size() < 2:
-            content.erase(data)
-    return content
+	var file : File = File.new()
+	file.open(source,File.READ)
+	var content : Array
+	while not file.eof_reached():
+		var line : PoolStringArray = file.get_csv_line(delimiter)
+		content.append(line)
+	file.close()
+	for data in content:
+		if data.size() < 2:
+			content.erase(data)
+	return content
 
 func structure_data(database : Array, are_values_columns : bool, x_values_index : int):
-    # @x_values_index can be either a column or a row relative to x values
-    # @y_values can be either a column or a row relative to y values
-    self.are_values_columns = are_values_columns
-    match are_values_columns:
-        true:
-            for row in database.size():
-                var t_vals : Array
-                for column in database[row].size():
-                    if column == x_values_index:
-                        var x_data = database[row][column]
-                        if x_data.is_valid_float() or x_data.is_valid_integer():
-                            x_datas.append(x_data as float)
-                        else:
-                            x_datas.append(x_data.replace(",",".") as float)
-                    elif column == z_values_index:
-                        var z_data = database[row][column]
-                        if z_data.is_valid_float() or z_data.is_valid_integer():
-                            z_datas.append(z_data as float)
-                        else:
-                            z_datas.append(z_data.replace(",",".") as float)
-                    else:
-                        if row != 0:
-                            var y_data = database[row][column]
-                            if y_data.is_valid_float() or y_data.is_valid_integer():
-                                t_vals.append(y_data as float)
-                            else:
-                                t_vals.append(y_data.replace(",",".") as float)
-                        else:
-                            y_labels.append(str(database[row][column]))
-                if not t_vals.empty():
-                    y_datas.append(t_vals)
-            x_label = str(x_datas.pop_front())
-            z_label = str(z_datas.pop_front())
-        false:
-            for row in database.size():
-                if row == x_values_index:
-                    x_datas = (database[row])
-                    x_label = x_datas.pop_front() as String
-                else:
-                    var values = database[row] as Array
-                    y_labels.append(values.pop_front() as String)
-                    y_datas.append(values)
-            for data in y_datas:
-                for value in data.size():
-                    data[value] = data[value] as float
-    
-    # draw y labels
-    var to_order : Array
-    var to_order_min : Array
-    for cluster in y_datas.size():
-        # define x_chors and y_chors
-        var ordered_cluster = y_datas[cluster] as Array
-        ordered_cluster.sort()
-        ordered_cluster = PoolIntArray(ordered_cluster)
-        var margin_max = ordered_cluster[ordered_cluster.size()-1]
-        var margin_min = ordered_cluster[0]
-        to_order.append(margin_max)
-        to_order_min.append(margin_min)
-    
-    to_order.sort()
-    to_order_min.sort()
-    var margin = to_order.pop_back()
-    if not origin_at_zero:
-        y_margin_min = to_order_min.pop_front()
-    v_dist = y_decim * pow(10.0,str(margin).length()-2)
-    var multi = 0
-    var p = (v_dist*multi) + ((y_margin_min) if not origin_at_zero else 0)
-    y_chors.append(p as String)
-    while p < margin:
-        multi+=1
-        p = (v_dist*multi) + ((y_margin_min) if not origin_at_zero else 0)
-        y_chors.append(p as String)
-    
-    # draw x_labels
-    if not show_x_values_as_labels:
-        to_order.clear()
-        to_order = x_datas as PoolIntArray
-        
-        to_order.sort()
-        margin = to_order.pop_back()
-        if not origin_at_zero:
-            x_margin_min = to_order.pop_front()
-        h_dist = x_decim * pow(10.0,str(margin).length()-2)
-        multi = 0
-        p = (h_dist*multi) + ((x_margin_min) if not origin_at_zero else 0)
-        x_labels.append(p as String)
-        while p < margin:
-            multi+=1
-            p = (h_dist*multi) + ((x_margin_min) if not origin_at_zero else 0)
-            x_labels.append(p as String)
+	# @x_values_index can be either a column or a row relative to x values
+	# @y_values can be either a column or a row relative to y values
+	self.are_values_columns = are_values_columns
+	match are_values_columns:
+		true:
+			for row in database.size():
+				var t_vals : Array
+				for column in database[row].size():
+					if column == x_values_index:
+						var x_data = database[row][column]
+						if x_data.is_valid_float() or x_data.is_valid_integer():
+							x_datas.append(x_data as float)
+						else:
+							x_datas.append(x_data.replace(",",".") as float)
+					elif column == z_values_index:
+						var z_data = database[row][column]
+						if z_data.is_valid_float() or z_data.is_valid_integer():
+							z_datas.append(z_data as float)
+						else:
+							z_datas.append(z_data.replace(",",".") as float)
+					else:
+						if row != 0:
+							var y_data = database[row][column]
+							if y_data.is_valid_float() or y_data.is_valid_integer():
+								t_vals.append(y_data as float)
+							else:
+								t_vals.append(y_data.replace(",",".") as float)
+						else:
+							y_labels.append(str(database[row][column]))
+				if not t_vals.empty():
+					y_datas.append(t_vals)
+			x_label = str(x_datas.pop_front())
+			z_label = str(z_datas.pop_front())
+		false:
+			for row in database.size():
+				if row == x_values_index:
+					x_datas = (database[row])
+					x_label = x_datas.pop_front() as String
+				else:
+					var values = database[row] as Array
+					y_labels.append(values.pop_front() as String)
+					y_datas.append(values)
+			for data in y_datas:
+				for value in data.size():
+					data[value] = data[value] as float
+	
+	# draw y labels
+	var to_order : Array
+	var to_order_min : Array
+	for cluster in y_datas.size():
+		# define x_chors and y_chors
+		var ordered_cluster = y_datas[cluster] as Array
+		ordered_cluster.sort()
+		ordered_cluster = PoolIntArray(ordered_cluster)
+		var margin_max = ordered_cluster[ordered_cluster.size()-1]
+		var margin_min = ordered_cluster[0]
+		to_order.append(margin_max)
+		to_order_min.append(margin_min)
+	
+	to_order.sort()
+	to_order_min.sort()
+	var margin = to_order.pop_back()
+	if not origin_at_zero:
+		y_margin_min = to_order_min.pop_front()
+	v_dist = y_decim * pow(10.0,str(margin).length()-2)
+	var multi = 0
+	var p = (v_dist*multi) + ((y_margin_min) if not origin_at_zero else 0)
+	y_chors.append(p as String)
+	while p < margin:
+		multi+=1
+		p = (v_dist*multi) + ((y_margin_min) if not origin_at_zero else 0)
+		y_chors.append(p as String)
+	
+	# draw x_labels
+	if not show_x_values_as_labels:
+		to_order.clear()
+		to_order = x_datas as PoolIntArray
+		
+		to_order.sort()
+		margin = to_order.pop_back()
+		if not origin_at_zero:
+			x_margin_min = to_order.pop_front()
+		h_dist = x_decim * pow(10.0,str(margin).length()-2)
+		multi = 0
+		p = (h_dist*multi) + ((x_margin_min) if not origin_at_zero else 0)
+		x_labels.append(p as String)
+		while p < margin:
+			multi+=1
+			p = (h_dist*multi) + ((x_margin_min) if not origin_at_zero else 0)
+			x_labels.append(p as String)
 
 func set_legend(l : Array):
-    legend = l
+	legend = l
 
 func get_legend():
-    return legend
+	return legend
 
 func apply_template(template_name : String):
-    template = template_name
-    templates = ECUtilities._load_templates()
-    if template_name!=null and template_name!="":
-        var custom_template = templates[template.to_lower()]
-        function_colors = custom_template.function_colors
-        v_lines_color = Color(custom_template.v_lines_color)
-        h_lines_color = Color(custom_template.h_lines_color)
-        box_color = Color(custom_template.outline_color)
-        font_color = Color(custom_template.font_color)
-    property_list_changed_notify()
+	template = template_name
+	templates = ECUtilities._load_templates()
+	if template_name!=null and template_name!="":
+		var custom_template = templates[template.to_lower()]
+		function_colors = custom_template.function_colors
+		v_lines_color = Color(custom_template.v_lines_color)
+		h_lines_color = Color(custom_template.h_lines_color)
+		box_color = Color(custom_template.outline_color)
+		font_color = Color(custom_template.font_color)
+	property_list_changed_notify()
 
 func _enter_tree():
-    _ready()
+	_ready()
