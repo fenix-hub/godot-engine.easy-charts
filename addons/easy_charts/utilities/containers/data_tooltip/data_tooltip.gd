f1b4d9c0a55021d6362e9344afdcaadb5cd78264
fenix-hub <nicolo.santilio@outlook.com>
2023-01-22 16:38:10 +0100

    🛠 data tooltip

--- addons/easy_charts/utilities/containers/data_tooltip/data_tooltip.gd
+++ addons/easy_charts/utilities/containers/data_tooltip/data_tooltip.gd
@@ -5,65 +5,32 @@ class_name DataTooltip
 var value : String = ""
 var position : Vector2 = Vector2()
 
-var OFFSET : Vector2 = Vector2(15,35)
-var GAP : Vector2 = Vector2(0,15)
-
-onready var Data : Label = $PointData/Value/x
-onready var Value : Label = $PointData/Value/y
-onready var Function : Label = $PointData/Function
+onready var x_lbl : Label = $PointData/x
+onready var y_lbl : Label = $PointData/Value/y
+onready var func_lbl : Label = $PointData/Value/Function
+onready var color_rect: Panel = $PointData/Value/Color
 
 func _ready():
 	hide()
+	update_size()
 
 func _process(delta):
-	if get_global_mouse_position().y > OFFSET.y + GAP.y:
-		rect_position = get_global_mouse_position() - OFFSET - GAP
-	else:
-		rect_position = get_global_mouse_position() + GAP*5 - OFFSET
+	if Engine.editor_hint:
+		return
+	rect_position = get_global_mouse_position() + Vector2(15, - (get_rect().size.y / 2))
 
-func update_datas(point : Control):
-	update_size()
-	
-	get("custom_styles/panel").set("bg_color",point.color)
-	
-	var font_color : Color
-	if point.color.g < 0.75:
-		font_color = Color(1,1,1,1)
-	else:
-		font_color = Color(0,0,0,1)
-	Data.set("custom_colors/font_color",font_color)
-	Value.set("custom_colors/font_color",font_color)
-	Function.set("custom_colors/font_color",font_color)
-	get("custom_styles/panel").set("border_color",font_color)
-	
-	Data.set_text(point.point_value[0]+":")
-	Value.set_text(point.point_value[1])
-	Function.set_text(point.function)
-	update()
-	show()
-
-func update_slice_datas(slice : Slice):
-	update_size()
-	
-	get("custom_styles/panel").set("bg_color",slice.color)
-	
-	var font_color : Color
-	if slice.color.g < 0.75:
-		font_color = Color(1,1,1,1)
-	else:
-		font_color = Color(0,0,0,1)
-	Data.set("custom_colors/font_color",font_color)
-	Value.set("custom_colors/font_color",font_color)
-	Function.set("custom_colors/font_color",font_color)
-	get("custom_styles/panel").set("border_color",font_color)
-	
-	Data.set_text(slice.x_value+":")
-	Value.set_text(slice.y_value)
-	Function.set_text(slice.function)
-	update()
-	show()
+func update_values(x: String, y: String, function: String, color: Color):
+	x_lbl.set_text(x)
+	y_lbl.set_text(y)
+	func_lbl.set_text(function)
+	color_rect.get("custom_styles/panel").set("bg_color", color)
 
 func update_size():
-	OFFSET.x = get_size().x/2
-	OFFSET.y = get_size().y 
-	GAP.y = OFFSET.y/3
+	x_lbl.set_text("")
+	y_lbl.set_text("")
+	func_lbl.set_text("")
+	rect_size = Vector2.ZERO
+
+func _on_DataTooltip_visibility_changed():
+	if not visible:
+		update_size()
