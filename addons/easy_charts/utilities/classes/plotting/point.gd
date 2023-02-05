5ca1f4870f423d5a994663979e4a55b7970d3a20
fenix-hub <nicolo.santilio@outlook.com>
2023-02-05 13:01:13 +0100

    (up) Point logic

--- addons/easy_charts/utilities/classes/plotting/point.gd
+++ addons/easy_charts/utilities/classes/plotting/point.gd
@@ -1,20 +1,12 @@
-tool
 extends Reference
 class_name Point
 
-enum Shape {
-	CIRCLE,
-	TRIANGLE,
-	SQUARE,
-	CROSS
-}
-
 var position: Vector2
-var value: Pair
+var value: Dictionary
 
-func _init(position: Vector2, value: Pair = Pair.new()) -> void:
-	self.value = value
+func _init(position: Vector2, value: Dictionary) -> void:
 	self.position = position
+	self.value = value
 
 func _to_string() -> String:
 	return "Value: %s\nPosition: %s" % [self.value, self.position]
