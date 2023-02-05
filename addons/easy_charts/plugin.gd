29a849453d275793fd07069328120d7d3137b68c
fenix-hub <nicolo.santilio@outlook.com>
2023-02-05 17:02:41 +0100

    (up) plugin

--- addons/easy_charts/plugin.gd
+++ addons/easy_charts/plugin.gd
@@ -2,8 +2,7 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-	add_autoload_singleton("ECUtilities","res://addons/easy_charts/utilities/scripts/ec_utilities.gd")
+	pass
 
 func _exit_tree():
-	remove_autoload_singleton("ECUtilities")
-
+	pass
