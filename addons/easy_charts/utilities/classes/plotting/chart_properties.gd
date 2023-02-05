b92b8e02ed6b57d9427f8a16102e35aad0b7bda8
fenix-hub <nicolo.santilio@outlook.com>
2023-02-05 13:02:14 +0100

    (up) chart properties

--- addons/easy_charts/utilities/classes/plotting/chart_properties.gd
+++ addons/easy_charts/utilities/classes/plotting/chart_properties.gd
@@ -4,57 +4,46 @@ class_name ChartProperties
 var title: String
 var x_label: String
 var y_label: String
-var functions_names: PoolStringArray
- 
+
 var x_scale: float = 5.0
 var y_scale: float = 2.0
+
+var x_tick_size: float = 7
+var x_ticklabel_space: float = 5
+var y_tick_size: float = 7
+var y_ticklabel_space: float = 5
 
 # Scale type, 0 = linear | 1 = logarithmic
 var x_scale_type: int = 0
 var y_scale_type: int = 0
 
-var borders: bool = true
-var frame: bool = true
-var background: bool = true
-var bounding_box: bool = true
-var grid: bool = false
-var ticks: bool = true
-var labels: bool = true
-var origin: bool = false
-var points: bool = true
+var draw_borders: bool = true
+var draw_frame: bool = true
+var draw_background: bool = true
+var draw_bounding_box: bool = true
+var draw_grid: bool = true
+var draw_ticks: bool = true
+var draw_origin: bool = false
+var draw_points: bool = true
+var show_labels: bool = true
+var show_legend: bool = true
 var interactive: bool = false
-
-var use_splines: bool = false
 
 var colors: Dictionary = {
 	frame = Color.whitesmoke,
-	borders = Color.red,
 	background = Color.white,
+	borders = Color.red,
 	bounding_box = Color.black,
-	labels = Color.black,
 	grid = Color.gray,
-	functions = ["#36a2eb", "#ff6384", "#ff9f40", "#ffcd56", "#4bc0c0"]
+	text = Color.black
 }
 
-var point_radius: float = 3.0
-var line_width: float = 1.0
-var bar_width: float = 10.0
-var shapes: Array = [Point.Shape.CIRCLE, Point.Shape.SQUARE, Point.Shape.TRIANGLE, Point.Shape.CROSS]
 var font_data: DynamicFontData = load("res://addons/easy_charts/utilities/assets/OpenSans-VariableFont_wdth,wght.ttf")
 var font: DynamicFont = DynamicFont.new()
 
 func _init() -> void:
 	font.set_font_data(font_data)
 	font.set_size(13)
-
-func get_function_color(function_index: int) -> Color:
-	return colors.functions[function_index] if function_index < colors.functions.size() else Color.black
-
-func get_function_name(function_idx: int) -> String:
-	return functions_names[function_idx] if function_idx < functions_names.size() else "Function %s" % function_idx
-
-func get_point_shape(function_index: int) -> int:
-	return shapes[function_index] if function_index < shapes.size() else Point.Shape.CIRCLE
 
 func get_string_size(text: String) -> Vector2:
 	return font.get_string_size(text)
