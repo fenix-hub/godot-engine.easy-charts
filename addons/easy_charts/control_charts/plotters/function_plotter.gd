1b8d7f2ceacac54ca132159fd26c122bef88c9fb
Nicolò Santilio <nicolo.santilio@outlook.com>
2024-05-06 09:39:03 +0200

    Fix/240505 (#103)
    * fix issue #97
    
    * implement function labels
    
    * fix #101
    
    * add visibility property. improves #41
    
    * fix #88 remove canvas reference to missing scene

--- addons/easy_charts/control_charts/plotters/function_plotter.gd
+++ addons/easy_charts/control_charts/plotters/function_plotter.gd
@@ -6,24 +6,27 @@ var x_domain: Dictionary
 var y_domain: Dictionary
 
 func _init(function: Function) -> void:
-	self.function = function
+    self.function = function
 
 func _ready() -> void:
-	set_process_input(get_chart_properties().interactive)
+    set_process_input(get_chart_properties().interactive)
 
 func update_values(x_domain: Dictionary, y_domain: Dictionary) -> void:
-	self.x_domain = x_domain
-	self.y_domain = y_domain
-	queue_redraw()
+    self.visible = self.function.get_visibility()
+    if not self.function.get_visibility():
+        return
+    self.x_domain = x_domain
+    self.y_domain = y_domain
+    queue_redraw()
 
 func _draw() -> void:
-	pass
+    return
 
 func get_box() -> Rect2:
-	return get_parent().get_parent().get_plot_box()
+    return get_parent().get_parent().get_plot_box()
 
 func get_chart_properties() -> ChartProperties:
-	return get_parent().get_parent().chart_properties
+    return get_parent().get_parent().chart_properties
 
 func get_relative_position(position: Vector2) -> Vector2:
-	return position - global_position
+    return position - global_position
--- addons/easy_charts/utilities/containers/data_tooltip/data_tooltip.gd
+++ addons/easy_charts/utilities/containers/data_tooltip/data_tooltip.gd
@@ -10,14 +10,14 @@ var gap: float = 15
 @onready var color_rect: Panel = $PointData/Value/Color
 
 func _ready():
-	hide()
-	update_size()
+    hide()
+    update_size()
 
 func update_position(position: Vector2) -> void:
-	if (position.x + gap + size.x) > get_parent().size.x:
-		self.position = position - Vector2(size.x + gap, (get_rect().size.y / 2))
-	else:
-		self.position = position + Vector2(15, - (get_rect().size.y / 2))
+    if (position.x + gap + size.x) > get_parent().size.x:
+        self.position = position - Vector2(size.x + gap, (get_rect().size.y / 2))
+    else:
+        self.position = position + Vector2(15, - (get_rect().size.y / 2))
 
 #func _process(delta):
 #	if Engine.editor_hint:
@@ -25,20 +25,20 @@ func update_position(position: Vector2) -> void:
 #	rect_position = get_global_mouse_position() + Vector2(15, - (get_rect().size.y / 2))
 
 func set_font(font: FontFile) -> void:
-	theme.set("default_font", font)
+    theme.set("default_font", font)
 
 func update_values(x: String, y: String, function_name: String, color: Color):
-	x_lbl.set_text(x)
-	y_lbl.set_text(y)
-	func_lbl.set_text(function_name)
-	color_rect.get("theme_override_styles/panel").set("bg_color", color)
+    x_lbl.set_text(x)
+    y_lbl.set_text(y)
+    func_lbl.set_text(function_name)
+    color_rect.get("theme_override_styles/panel").set("bg_color", color)
 
 func update_size():
-	x_lbl.set_text("")
-	y_lbl.set_text("")
-	func_lbl.set_text("")
-	size = Vector2.ZERO
+    x_lbl.set_text("")
+    y_lbl.set_text("")
+    func_lbl.set_text("")
+    size = Vector2.ZERO
 
 func _on_DataTooltip_visibility_changed():
-	if not visible:
-		update_size()
+    if not visible:
+        update_size()
