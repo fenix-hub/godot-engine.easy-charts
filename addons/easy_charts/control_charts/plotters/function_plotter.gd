168745cdc45cbaeae8c35a2ef80a9d5d9b588004
namelessvoid <simon_kerler@web.de>
2025-03-07 18:47:32 +0100

    Move plotter creation into factory method

--- addons/easy_charts/control_charts/plotters/function_plotter.gd
+++ addons/easy_charts/control_charts/plotters/function_plotter.gd
@@ -5,28 +5,41 @@ var function: Function
 var x_domain: Dictionary
 var y_domain: Dictionary
 
+static func create_for_function(function: Function) -> FunctionPlotter:
+	match function.get_type():
+		Function.Type.LINE:
+			return LinePlotter.new(function)
+		Function.Type.AREA:
+			return AreaPlotter.new(function)
+		Function.Type.PIE:
+			return PiePlotter.new(function)
+		Function.Type.BAR:
+			return BarPlotter.new(function)
+		Function.Type.SCATTER, _:
+			return ScatterPlotter.new(function)
+
 func _init(function: Function) -> void:
-    self.function = function
+	self.function = function
 
 func _ready() -> void:
-    set_process_input(get_chart_properties().interactive)
+	set_process_input(get_chart_properties().interactive)
 
 func update_values(x_domain: Dictionary, y_domain: Dictionary) -> void:
-    self.visible = self.function.get_visibility()
-    if not self.function.get_visibility():
-        return
-    self.x_domain = x_domain
-    self.y_domain = y_domain
-    queue_redraw()
+	self.visible = self.function.get_visibility()
+	if not self.function.get_visibility():
+		return
+	self.x_domain = x_domain
+	self.y_domain = y_domain
+	queue_redraw()
 
 func _draw() -> void:
-    return
+	return
 
 func get_box() -> Rect2:
-    return get_parent().get_parent().get_plot_box()
+	return get_parent().get_parent().get_plot_box()
 
 func get_chart_properties() -> ChartProperties:
-    return get_parent().get_parent().chart_properties
+	return get_parent().get_parent().chart_properties
 
 func get_relative_position(position: Vector2) -> Vector2:
-    return position - global_position
+	return position - global_position
