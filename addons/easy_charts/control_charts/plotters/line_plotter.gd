5b4fa9d48203421691f3b286b5a993eee1500e2e
namelessvoid <simon_kerler@web.de>
2025-05-13 17:04:57 +0200

    refac: Improve variable name

--- addons/easy_charts/control_charts/plotters/line_plotter.gd
+++ addons/easy_charts/control_charts/plotters/line_plotter.gd
@@ -4,22 +4,23 @@ class_name LinePlotter
 func _get_spline_points(density: float = 10.0, tension: float = 1) -> PackedVector2Array:
 	var spline_points: PackedVector2Array = []
 	
-	var point_positions: PackedVector2Array = points_positions
+	var augmented_positions: PackedVector2Array = points_positions
 	var pi: Vector2 = points_positions[0] - Vector2(10, -10)
-	var pf: Vector2 = point_positions[point_positions.size() - 1] + Vector2(10, 10)
+	var pf: Vector2 = augmented_positions[augmented_positions.size() - 1] + Vector2(10, 10)
 
-	point_positions.insert(0, pi)
-	point_positions.push_back(pf)
+	augmented_positions.insert(0, pi)
+	augmented_positions.push_back(pf)
 	
-	for p in range(1, point_positions.size() - 2, 1) : #(inclusive)
+	for p in range(1, augmented_positions.size() - 2, 1) : #(inclusive)
 		for f in range(0, density + 1, 1):
 			spline_points.append(
-				point_positions[p].cubic_interpolate(
-					point_positions[p + 1], 
-					point_positions[p - 1], 
-					point_positions[p + 2], 
-					f / density)
+				augmented_positions[p].cubic_interpolate(
+					augmented_positions[p + 1],
+					augmented_positions[p - 1],
+					augmented_positions[p + 2],
+					f / density
 				)
+			)
 	
 	return spline_points
 
