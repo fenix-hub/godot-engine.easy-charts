1c76abcb41bdd1b52a2c0d06abbe8d7d5bc9b36c
namelessvoid <simon_kerler@web.de>
2025-08-23 13:41:16 +0200

    Fix missing var

--- addons/easy_charts/examples/bar_chart/bar_chart_example.gd
+++ addons/easy_charts/examples/bar_chart/bar_chart_example.gd
@@ -13,7 +13,7 @@ func _ready():
 	
 	# Let's customize the chart properties, which specify how the chart
 	# should look, plus some additional elements like labels, the scale, etc...
-	cp = ChartProperties.new()
+	var cp = ChartProperties.new()
 	cp.y_scale = 10
 	cp.draw_origin = true
 	cp.draw_bounding_box = false
